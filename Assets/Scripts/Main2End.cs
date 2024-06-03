c3d046b60c4030a48fa321964a55a98ac69e5c75
NiimiSyunya <niimicomputergraphics@gmail.com>
2024-06-03 18:23:17 +0900

    main-Scene to end-Scene

--- Assets/Scripts/Main2End.cs
+++ Assets/Scripts/Main2End.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Main2End : MonoBehaviour
 {
+    public TimeCounter timeCounter;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+    }
+
+    void Update()
+    {
+        // 経過時間が0になったらシーンを変更
+        if (timeCounter.countdownSec <= 0)
+        {
+            ChangeScene();
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    void ChangeScene()
     {
-        
+        SceneManager.LoadScene("end");
     }
 }
