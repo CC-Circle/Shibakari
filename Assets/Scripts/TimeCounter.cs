be5145ec61110609e5fec69a1c334739ce449a12
Niimi Syunya <niimicomputergraphics@gmail.com>
2024-08-21 02:36:55 +0900

    Add Finish Text

--- Assets/Scripts/TimeCounter.cs
+++ Assets/Scripts/TimeCounter.cs
@@ -3,21 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro; // TextMeshProを使用するための名前空間
-using UnityEngine.SceneManagement; // シーン管理の名前空間
+using UnityEngine.SceneManagement;
+using UnityEditor.Rendering;
+using UnityEditor.UIElements; // シーン管理の名前空間
 
 public class TimeCounter : MonoBehaviour
 {
     public float CountTime = 40; // カウントダウンの秒数を設定
     [SerializeField] private Image uiFill;
     [SerializeField] private TextMeshProUGUI uiText;
-
     public GameObject SceneManager; // シーンマネージャーのゲームオブジェクト
     private MySceneManager mySceneManager; // シーンマネージャースクリプトのインスタンス
+    public GameObject[] UiElements; // UIのゲームオブジェクト
+    private float PauseTime = 0; // 一時停止時間を計測するための変数
+    [SerializeField] public GameObject FinishText; // ゲーム終了時に表示するテキスト
 
     // Startは最初のフレームの前に一度だけ呼び出される
     void Start()
     {
         mySceneManager = SceneManager.GetComponent<MySceneManager>(); // SceneManagerオブジェクトにアタッチされているMySceneManagerスクリプトを取得
+        UiElements = GameObject.FindGameObjectsWithTag("UI");
+        FinishText.SetActive(false); // ゲーム終了時に表示するテキストを非表示
+
     }
 
     // Updateは毎フレーム呼び出される
@@ -31,6 +38,29 @@ public class TimeCounter : MonoBehaviour
         uiFill.fillAmount = Mathf.InverseLerp(0, 40, CountTime);
 
         if (CountTime <= 0) // カウントが0になったら
+        {
+            PauseTime += Time.deltaTime; // 一時停止時間の計測開始
+            // Debug.Log(PauseTime);
+
+            foreach (GameObject UiElement in UiElements)
+            {
+                CanvasGroup canvasGroup = UiElement.GetComponent<CanvasGroup>();
+                if (canvasGroup == null)
+                {
+                    // CanvasGroupがアタッチされていない場合、追加する
+                    canvasGroup = UiElement.AddComponent<CanvasGroup>();
+                }
+                // UIの透明度を0にして、インタラクティブとレイキャストを無効化
+                // setActive(false)で実装すると，うまくいかなかったので，CanvasGroupを使って透明度を変更する方法を採用
+                canvasGroup.alpha = 0f;
+                canvasGroup.interactable = false;
+                canvasGroup.blocksRaycasts = false;
+            }
+            FinishText.SetActive(true); // ゲーム終了時に表示するテキストを表示
+        }
+
+        // CountTimeのみでも可能だが，可読性向上のために，PauseTimeを使って条件分岐
+        if (PauseTime >= 5)
         {
             mySceneManager.flag = true; // MySceneManagerスクリプトを有効化
         }
