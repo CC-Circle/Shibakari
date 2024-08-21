82951f165d1a0abc853934b3db0928d082651237
shiruku-shibata <104050688+shiruku-shibata@users.noreply.github.com>
2024-08-21 13:45:35 +0900

    Revert "モグラの出現"

--- Assets/Scripts/AutoDestroyAndSpawnGrass.cs
+++ Assets/Scripts/AutoDestroyAndSpawnGrass.cs
@@ -4,12 +4,7 @@ using System.Collections;
 public class AutoDestroyAndSpawn : MonoBehaviour
 {
     public float lifetime = 5f; // オブジェクトが破壊されるまでの時間（秒）
-
-    [Header("草")]
-    public GameObject spawnPrefab_grass; // 破壊後に生成されるオブジェクトのプレハブ
-
-    [Header("もぐら")]
-    public GameObject spawnPrefab_mogura; // 1％の確率で生成される異なるオブジェクトのプレハブ
+    public GameObject spawnPrefab; // 破壊後に生成されるオブジェクトのプレハブ
 
     private Vector3 initialPosition;
 
@@ -28,28 +23,12 @@ public class AutoDestroyAndSpawn : MonoBehaviour
         yield return new WaitForSeconds(lifetime);
 
         // オブジェクトを破壊
-        //Destroy(gameObject);
+        Destroy(gameObject);
         Debug.Log("Destroyed " + gameObject.name + " after " + lifetime + " seconds");
 
-        // 1％の確率で異なるオブジェクトを生成
-        GameObject objToSpawn;
-        if (Random.value <= 0.01f) // 1％の確率
-        {
-            objToSpawn = spawnPrefab_mogura;
-            // オブジェクトが地面に埋まらないように少し浮かせる    
-            initialPosition.y = 13.5f;
-
-            // 新しいオブジェクトを初期位置に生成
-            Instantiate(objToSpawn, initialPosition, Quaternion.Euler(0, -2, 0));
-        }
-        else
-        {
-            objToSpawn = spawnPrefab_grass;
-            // オブジェクトが地面に埋まらないように少し浮かせる    
-            initialPosition.y = 13.5f;
-
-            // 新しいオブジェクトを初期位置に生成
-            Instantiate(objToSpawn, initialPosition, Quaternion.Euler(270, 0, 0));
-        }
+        // 新しいオブジェクトを初期位置に生成
+        initialPosition.y = 13.5f; // オブジェクトが地面に埋まらないように少し浮かせる    
+        Instantiate(spawnPrefab, initialPosition, Quaternion.Euler(270, 0, 0));
+        Debug.Log("Spawned new object at " + initialPosition);
     }
 }
